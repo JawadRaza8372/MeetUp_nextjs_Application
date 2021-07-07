c048ae45075dde1bb68e8e1bedd061ef4cc7bad1
jawadraza8372 <jawdraza8372@gmail.com>
2021-07-07 17:32:34 +0500

    fixingerror

--- Components/NewMeetup/MeetupForm.js
+++ Components/NewMeetup/MeetupForm.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 function MeetupForm({submitFun}) {
-    const [state, setstate] = useState(null)
+    const [state, setstate] = useState({imaglink:'https://picsum.photos/200/300?random='})
     let subitfunction=(e)=>{
         e.preventDefault();
         submitFun(state);
@@ -23,14 +23,13 @@ function MeetupForm({submitFun}) {
             <form onSubmit={subitfunction} style={{minWidth:"30%",maxWidth:"80%",border:"1px solid #0d6efd ",overflow:"hidden",borderRadius:"10px"}}>
             <div className="row" style={{background:"#0d6efd"}}><h3 style={{textAlign:"center",color:"white",textDecoration:"underlined"}}>Add New MeetUp</h3></div>
  <div style={{width:"100%",textAlign:"center"}}>
- <input id="title" onChange={handle} type="text" placeholder="MeetUp Heading"/>
+ <input id="title" onChange={handle} type="text" placeholder="MeetUp Heading"  required autoComplete="off"/>
  <br/>
-            <input id="description" onChange={handle} type="text" placeholder="MeetUp description"/>
+            <input id="description" onChange={handle} type="text" placeholder="MeetUp description"  required autoComplete="off"/>
 
             <br/>
 
- <input id="address" onChange={handle} type="text" placeholder="Address"/>
- <input id="imaglink" onChange={handle} type="text" placeholder="Image Link"/>
+ <input id="address" onChange={handle} type="text" placeholder="Address" required autoComplete="off"/>
 
  <br/>
  <button type="submit" className="btn btn-outline-primary m-3">Post</button>
